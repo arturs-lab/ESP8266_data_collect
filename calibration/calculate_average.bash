577c9f280e74d9974979a0fc22eac3566cb799a3
arturj <arturj@bkpsrvr1.qq.com>
2022-11-24 16:26:34 -0600

    calculations script

---
+++ calibration/calculate_average.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for TYPE in h t; do
+  echo -n "${TYPE} "
+  for f in 180 183 184 185 186 187; do
+    #echo -n "${ID} ${TYPE} "
+    # This format is good for copy-pasting into spreadsheet
+    grep "172.16.7.${ID}.*type=${TYPE}" ${1} | grep -v nan | sed -e 's/.*raw=//;s/ .*//' | awk '{ s+=$1; n += 1 } END { printf "%.5f\n", s/n }' ;
+# this is more informative for testing
+#printf "Samples:%d Total:%.5f Average:%.5f\n", n, s, s/n }' ;
+  done
+done
+
