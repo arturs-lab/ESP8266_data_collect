c42ae42fb1362af2429950ecdf39eb135e0a7350
arturj <arturj@bkpsrvr1.qq.com>
2022-11-24 16:31:28 -0600

    correct typo

--- calibration/calculate_average.bash
+++ calibration/calculate_average.bash
@@ -2,7 +2,7 @@
 
 for TYPE in h t; do
   echo -n "${TYPE} "
-  for f in 180 183 184 185 186 187; do
+  for ID in 180 183 184 185 186 187; do
     #echo -n "${ID} ${TYPE} "
     # This format is good for copy-pasting into spreadsheet
     grep "172.16.7.${ID}.*type=${TYPE}" ${1} | grep -v nan | sed -e 's/.*raw=//;s/ .*//' | awk '{ s+=$1; n += 1 } END { printf "%.5f\n", s/n }' ;
